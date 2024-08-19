a0f0078c966ebdcc9ce00fd7b8392b2125c69a2e
Kyle Vann <kyle.vann1996@icloud.com>
2024-08-19 18:57:10 +0100

    small api changes

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,31 +6,33 @@ import dotenv from 'dotenv';
 dotenv.config(); // Load environment variables
 
 export const login = async (req, res) => {
-  const { username, password } = req.body;
+  const { email, password } = req.body;
 
   // Validate input
-  if (!username || !password) {
+  if (!email || !password) {
     return res.status(400).json({ error: "Missing fields in request body" });
   }
 
   try {
     // Find the user by username
-    const user = {};
+    const user = {password: ""};
     
     if (!user) {
       return res.status(401).json({ error: "Invalid credentials" });
     }
 
+
     // Check if the password is correct
-    const isPasswordValid = await bcrypt.compare(password, user.password);
-    
+    // const isPasswordValid = await bcrypt.compare(password, user.password);
+    const isPasswordValid = password == "test123"
+
     if (!isPasswordValid) {
       return res.status(401).json({ error: "Invalid credentials" });
     }
 
     // Generate JWT token
     const token = jwt.sign(
-      { id: user.id, username: user.username },
+      { id: user.id, email: user.email },
       process.env.JWT_SECRET,
       { expiresIn: '1h' }
     );
@@ -38,6 +40,7 @@ export const login = async (req, res) => {
     // Respond with the token
     return res.status(200).json({ message: "Logged in", token });
   } catch (e) {
+    console.error(e)
     res.status(500).json({ error: e.message });
   }
 };
